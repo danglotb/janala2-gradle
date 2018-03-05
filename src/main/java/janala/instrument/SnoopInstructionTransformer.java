e819b186b136f78396c6564d78f77108735825e4
danglotb <bdanglot@gmail.com>
2018-03-05 18:47:49 +0100

    feat: uses the configuration rather than hardcoded values

--- src/main/java/janala/instrument/SnoopInstructionTransformer.java
+++ src/main/java/janala/instrument/SnoopInstructionTransformer.java
@@ -26,9 +26,7 @@ public class SnoopInstructionTransformer implements ClassFileTransformer {
 
   /** packages that should be exluded from the instrumentation */
   private static boolean shouldExclude(String cname) {
-    String[] exclude = {"com/sun", "sun", "java", "jdk", "com/google/monitoring",
-                        "janala", "dk/brics"};
-    for (String e : exclude) {
+    for (String e : Config.instance.excludeList) {
       if (cname.startsWith(e)) {
         return true;
       }
